c65593b46c62bf53b4f954a18697ab3063890c8e
jnrepo <joon.heo08@gmail.com>
2018-09-11 14:00:48 -0700

    init

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list rentals');
+
+test('should show rentals as the home page', function (assert) {} );
+test('should link to information about the company.', function (assert) {} );
+test('should link to contact information.', function (assert) {} );
+test('should list available rentals.', function (assert) {} );
+test('should filter the list of rentals by city.', function (assert) {} );
+test('should show details for a selected rental', function (assert) {} );
